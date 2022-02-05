e5a0cd82792336716587ccf7374416b75b77ec56
scodlez <98987101+scodlez@users.noreply.github.com>
2022-02-05 22:02:10 +0300

    Главный файл

---
+++ vimeworld_match.py
@@ -0,0 +1,69 @@
+import requests, json, time, os
+import colorama
+from colorama import Fore
+colorama.init()
+
+global nicknames
+nicknames = []
+match = input("Введите ID матча: ")
+os.system("cls")
+
+#
+#Работа с файлом
+#
+
+if not os.path.isdir("matches"):
+     os.mkdir("matches")
+match_file = open(match + ".txt", 'w')
+match_file.close()
+
+#Отображение хуйни в консоли
+actualmatch = json.loads(requests.get("https://api.vimeworld.ru/match/" + match).text)
+print(Fore.GREEN, "Игра - ", Fore.WHITE, actualmatch["game"])
+print(Fore.GREEN, "Карта - ", Fore.WHITE, actualmatch["mapName"])
+print(Fore.GREEN, "Сервер - ", Fore.WHITE, actualmatch["server"])
+print(Fore.GREEN, "Игра - ", Fore.WHITE, "https://vimetop.ru/matches#" + match)
+teamWIN = actualmatch["winner"]["team"]
+if teamWIN == "red":
+    print(Fore.GREEN, "Команда победителей -", Fore.RED, "Красные")
+else:
+    print(Fore.GREEN, "Команда победителей -", Fore.CYAN, "Синие")
+
+print("\n\n", Fore.WHITE)
+print("Команда -", Fore.CYAN, "Синие")
+for rand in range(4):
+    usernick = json.loads(
+        requests.get(
+            "https://api.vimeworld.ru/user/"
+            + str(actualmatch["teams"][1]["members"][rand])
+        ).text
+    )[0]["username"]
+    donate = json.loads(
+        requests.get(
+            "https://api.vimeworld.ru/user/"
+            + str(actualmatch["teams"][1]["members"][rand])
+        ).text
+    )[0]["rank"]
+    print(Fore.YELLOW, donate, Fore.WHITE, usernick)
+print("\n")
+print("Команда -", Fore.RED, "Красные")
+for rand1 in range(4):
+    usernick = json.loads(
+        requests.get(
+            "https://api.vimeworld.ru/user/"
+            + str(actualmatch["teams"][0]["members"][rand1])
+        ).text
+    )[0]["username"]
+    donate = json.loads(
+        requests.get(
+            "https://api.vimeworld.ru/user/"
+            + str(actualmatch["teams"][0]["members"][rand1])
+        ).text
+    )[0]["rank"]
+    print(Fore.YELLOW, donate, Fore.WHITE, usernick)
+
+#Конец =)
+print("" * 3) #Пробел
+print(Fore.GREEN, "Нажмите на любую, чтобы перезагрузить программу")
+input()
+os.system("start vimeworld_match.exe")
